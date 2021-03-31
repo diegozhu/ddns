e8639922c4903a8af6566950813a89f0fbce4fe1
diegozhu <674417307@qq.com>
2021-03-31 12:51:53 +0800

    update

--- ddns.js
+++ ddns.js
@@ -1,31 +1,22 @@
 const { networkInterfaces, hostname } = require('os'),
   { execSync } = require('child_process'),
-  logfile = 'C:\\ddnslog.log',
-  fs = require('fs'),
-  args = process.argv.slice(2);
+  args = process.argv.slice(2),
+  fs = require('fs');
 
 process.env.LOGIN_TOKEN = args[0];
 process.env.DOMAIN_ID = args[1];
- 
+
 const updateDns = require('dnspod-import-core'),
 	log = (str)=>{
 	  const text = `${new Date().toISOString().substring(0, 19).replace('T', ' ')} ${typeof str === 'string' ? str : JSON.stringify(str)}`;
-	  fs.appendFileSync(logfile, text + '\r\n');
 	  console.log(text);
 	};
 
 log('');
 log('');
 log(process.argv.slice(2));
-
-process.on('uncaughtException', function(err){
-	try{
-		log(err.message);
-		log(err.stack);
-	}catch(e){
-		console.error(e);
-	}
-});
+log('process.env.LOGIN_TOKEN:' + process.env.LOGIN_TOKEN);
+log('process.env.DOMAIN_ID:' + process.env.DOMAIN_ID + ' '+ process.env.DOMAIN_ID.length);
 
 const nets = networkInterfaces();
 const res = [];
@@ -42,15 +33,10 @@ if (ipv6) {
   const data = {};
   data[name] = ipv6;
   log(data);
-  try {
-	  if(!args[0] || !args[0]){
-		log('please provide dnspod login token and domain id');
-	  }else{
-		updateDns(data); 
-	  }
-  } catch (e) {
-	log('error');
-    log(e);
+  if(!args[0] || !args[0]){
+	throw new Error('please provide dnspod login token and domain id');
+  }else{
+	updateDns(data); 
   }
 } else {
   log('could not find IPv6 address');
