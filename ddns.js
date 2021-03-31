a927e64bc5eeb9419561a426d2726fa024997940
diegozhu <674417307@qq.com>
2021-03-31 10:32:11 +0800

    asdf

--- ddns.js
+++ ddns.js
@@ -1,15 +1,20 @@
 const { networkInterfaces, hostname } = require('os'),
+  { execSync } = require('child_process'),
   logfile = 'C:\\ddns.log',
   fs = require('fs'),
-  args = process.argv.slice(2),
-  { execSync } require('child_process');
-
-console.log(process.argv.slice(2));
+  args = process.argv.slice(2);
 
 process.env.LOGIN_TOKEN = args[0];
 process.env.DOMAIN_ID = args[1];
+ 
+const updateDns = require('dnspod-import-core'),
+	log = (str)=>{
+	  const text = `${new Date().toISOString().substring(0, 19).replace('T', ' ')} ${JSON.stringify(str)}`;
+	  fs.appendFileSync(logfile, text + '\r\n');
+	  console.log(text);
+	};
 
-const updateDns = require('dnspod-import-core');
+log(process.argv.slice(2));
 
 const nets = networkInterfaces();
 const res = [];
@@ -22,25 +27,22 @@ const ipv6 = res.filter((e) => e.length)[0][0].address,
   name = args[2] || hostname().toLowerCase();
 
 if (ipv6) {
-  console.log(`${name}: ${ipv6 || ''}`);
+  log(`${name}: ${ipv6 || ''}`);
   const data = {};
   data[name] = ipv6;
-  console.log(data);
-  fs.appendFileSync(logfile, `${new Date().toISOString().substring(0, 19).replace('T', ' ')} ${JSON.stringify(data)}`);
+  log(data);
   try {
 	  if(!args[0] || !args[0]){
-		console.log('please provide dnspod login token and domain id');
+		log('please provide dnspod login token and domain id');
 	  }else{
 		updateDns(data); 
 	  }
   } catch (e) {
-    console.error(e);
-    fs.appendFileSync(logfile, `${new Date().toISOString().substring(0, 19).replace('T', ' ')} ${JSON.stringify(e)}`);
+    log(e);
   }
 } else {
-  fs.appendFileSync(logfile, `${new Date().toISOString().substring(0, 19).replace('T', ' ')} could not find IPv6 address`);
+  log('could not find IPv6 address');
 }
 
-console.log('check ddns update');
-execSync('git pull', {stdio: 'inherit'})
+log('check ddns update');
 
